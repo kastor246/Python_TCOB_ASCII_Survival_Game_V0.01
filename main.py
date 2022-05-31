6dec067e26f88f26b1a4767952db06594a05c9b0
kastor246 <kastor2468@gmail.com>
2022-05-30 20:59:07 -0400

    V0.02 Of my first game.
    Signed-off-by: kastor246 <kastor2468@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tcod
 
-from actions import EscapeAction, MovementAction
+from engine import Engine
+from entity import Entity
 from input_handlers import EventHandler
 
 
@@ -8,14 +9,16 @@ def main() -> None:
     screen_width = 100
     screen_height = 70
 
-    player_x = int(
-        screen_width / 2)  # Places the Player in the middle of the screen, the int() ensures TCOD doesn't throw an error in case of a float
-    player_y = int(screen_height / 2)
-
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
     event_handler = EventHandler()
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
+
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
 
     with tcod.context.new_terminal(
             screen_width,
@@ -27,23 +30,8 @@ def main() -> None:
         root_console = tcod.Console(screen_width, screen_height,
                                     order="F")  # Creates our "console" which is what we draw to.
         while True:
-            root_console.print(x=player_x, y=player_y, string="@")
-
-            context.present(root_console)  # Updates the screen
-
-            root_console.clear()
-
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
-
-                if action is None:
-                    continue
-                if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
-
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
+            engine.render(console=root_console, context=context)
+            events = tcod.event.wait()
 
 
 if __name__ == "__main__":
