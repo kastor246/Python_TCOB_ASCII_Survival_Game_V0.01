c201fa2587d13e5d84dee74c39bf6c37e7141ba2
kastor246 <kastor2468@gmail.com>
2022-06-02 16:42:08 -0400

    V0.04 Of my first game.
    Signed-off-by: kastor246 <kastor2468@gmail.com>

--- engine.py
+++ engine.py
@@ -1,35 +1,54 @@
-from typing import Set, Iterable, Any
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
-from actions import Action
-from entity import Entity
-from game_map import GameMap
-from input_handlers import EventHandler
+from input_handlers import MainGameEventHandler
+from render_functions import render_bar
+
+if TYPE_CHECKING:
+    from entity import Actor
+    from game_map import GameMap
+    from input_handlers import EventHandler
 
 
 class Engine:
-    def __init__(self, entities: Set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
-        self.entities = entities
-        self.event_handler = event_handler
-        self.game_map = game_map
+    game_map: GameMap
+
+    def __init__(self, player: Actor):
+        self.event_handler: EventHandler = MainGameEventHandler(self)
         self.player = player
 
-    def handle_events(self, events: Iterable[Any]) -> None:
-        for event in events:
-            action = self.event_handler.dispatch(event)
 
-            if action is None:
-                continue
+    def handle_enemy_turns(self) -> None:
+        for entity in set(self.game_map.actors) - {self.player}:
+            if entity.ai:
+                entity.ai.perform()
 
-            action.perform(self, self.player)
+
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the players point of view"""
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8
+        )
+        # If a tile is "visible it should be added to "explored"
+        self.game_map.explored |= self.game_map.visible
 
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
 
-        for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+        render_bar(
+            console=console,
+            current_value=self.player.fighter.hp,
+            maximum_value=self.player.fighter.max_hp,
+            total_width=20,
+        )
+
         context.present(console)
 
         console.clear()
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import copy
+
 import tcod
 
 from engine import Engine
-from entity import Entity
+import entity_factories
 from procgen import generate_dungeon
-from input_handlers import EventHandler
 
 
 def main() -> None:
@@ -17,25 +18,26 @@ def main() -> None:
     room_min_size = 6
     max_rooms = 30
 
+    max_monsters_per_room = 2
+
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
-    event_handler = EventHandler()
+    player = copy.deepcopy(entity_factories.player)
+    engine = Engine(player=player)
 
-    player = Entity(int(screen_width // 2), int(screen_height // 2), "@", (255, 255, 255))
-    npc = Entity(int(screen_width // 2 - 5), int(screen_height // 2), "@", (255, 255, 0))
-    entities = [npc, player]
 
-    game_map = generate_dungeon(
+
+    engine.game_map = generate_dungeon(
         max_rooms=max_rooms,
         room_max_size=room_max_size,
         room_min_size=room_min_size,
         map_width=map_width,
         map_height=map_height,
-        player=player
+        max_monsters_per_room = max_monsters_per_room,
+        engine=engine
     )
-
-    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
+    engine.update_fov()
 
     with tcod.context.new_terminal(
             screen_width,
@@ -48,8 +50,7 @@ def main() -> None:
                                     order="F")  # Creates our "console" which is what we draw to.
         while True:
             engine.render(console=root_console, context=context)
-            events = tcod.event.wait()
-            engine.handle_events(events)
+            engine.event_handler.handle_events()
 
 
 
